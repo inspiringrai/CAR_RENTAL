34858887a328efa1723dbb4ddcedc10be980ad47
Parth Patel <Parth.DP@iiitb.ac.in>
2022-05-08 18:04:52 +0530

    updated docker-compose, fetch url of Database in application properties

--- Server/src/test/java/com/spe/carRental/Spe1ApplicationTests.java
+++ Server/src/test/java/com/spe/carRental/Spe1ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.spe.carRental;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Spe1ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.spe.carRental;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Spe1ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
